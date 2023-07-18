a4a0108c5fadf1a8ec3cb4a725d888a04d963902
Kiran Anand <liteninkiran@hotmail.com>
2023-07-18 13:01:42 +0100

    05 - Adding login & authentication: Add second form

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -7,8 +7,8 @@
             Enter your phone number
         </h1>
 
-        <!-- Form -->
-        <form action="#" @submit.prevent="handleLogin">
+        <!-- Login Form -->
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
 
             <div
                 class="overflow-hidden
@@ -73,6 +73,72 @@
 
         </form>
 
+        <!-- Verification Form -->
+        <form v-else action="#" @submit.prevent="handleVerification">
+
+            <div
+                class="overflow-hidden
+                        shadow
+                        sm:rounded-md
+                        max-w-sm
+                        mx-auto
+                        text-left"
+            >
+
+                <!-- Phone Number -->
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input
+                            v-maska
+                            data-maska="######"
+                            v-model="credentials.login_code"
+                            type="text"
+                            name="login_code"
+                            id="login_code"
+                            placeholder="123456"
+                            class="mt-1
+                                    block
+                                    w-full
+                                    px-3
+                                    py-2
+                                    rounded-md
+                                    border
+                                    shadow-sm
+                                    focus:outline-none
+                                    border-gray-300
+                                    focus:border-black"
+                        >
+                    </div>
+                </div>
+
+                <!-- Submit -->
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                        @submit.prevent="handleVerification"
+                        type="submit"
+                        class="inline-flex 
+                                justify-center 
+                                rounded-md 
+                                border 
+                                border-transparent 
+                                py-2 
+                                px-4 
+                                text-sm 
+                                font-medium 
+                                shadow-sm 
+                                focus:outline-none
+                                bg-black 
+                                hover:bg-gray-600 
+                                text-white"
+                    >
+                        Verify
+                    </button>
+                </div>
+
+            </div>
+
+        </form>
+
     </div>
 
 </template>
@@ -80,16 +146,31 @@
 <script setup>
 
     import { vMaska } from 'maska';
-    import { reactive } from 'vue';
+    import { reactive, ref, onMounted } from 'vue';
+    import { useRouter } from 'vue-router';
     import axios from 'axios'
+
+    const router = useRouter()
 
     const credentials = reactive({
         phone: null,
+        login_code: null,
     });
+
+    const waitingOnVerification = ref(false)
+
+    onMounted(() => {
+        if (localStorage.getItem('token')) {
+            router.push({
+                name: 'landing',
+            })
+        }
+    })
 
     const getFormattedCredentials = () => {
         return {
             phone: credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '').replace('-', ''),
+            login_code: credentials.login_code,
         }
     }
 
@@ -97,12 +178,29 @@
         axios.post('http://localhost:8000/api/login', getFormattedCredentials())
             .then((response) => {
                 console.log(response.data);
+                waitingOnVerification.value = true;
             })
             .catch((error) => {
                 console.error(error);
                 alert(error.response.data.message);
             })
     };
+
+
+    const handleVerification = () => {
+        axios.post('http://localhost:8000/api/login/verify', getFormattedCredentials())
+            .then((response) => {
+                console.log(response.data); // Auth token is returned
+                localStorage.setItem('token', response.data);
+                router.push({
+                    name: 'landing',
+                });
+            })
+            .catch((error) => {
+                console.error(error)
+                alert(error.response.data.message)
+            })
+    }
 
 </script>
 
